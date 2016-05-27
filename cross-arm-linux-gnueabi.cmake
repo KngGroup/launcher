7efab44fcd00f155ef75b8b917a90c709bdbf513
Dmitry Bykov <dmitry.bykov@sibers.com>
2016-05-27 14:02:50 +0600

    added cross-arm-linux-gnueabi.cmake

---
+++ cross-arm-linux-gnueabi.cmake
@@ -0,0 +1,19 @@
+#
+# CMake Toolchain file for crosscompiling on ARM.
+#
+# This can be used when running cmake in the following way:
+#  cd build/
+#  cmake .. -DCMAKE_TOOLCHAIN_FILE=../cross-arm-linux-gnueabi.cmake
+#
+
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_SYSROOT /home/bykov/tau)
+
+set(CMAKE_C_COMPILER "gcc")
+set(CMAKE_CXX_COMPILER "g++")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
